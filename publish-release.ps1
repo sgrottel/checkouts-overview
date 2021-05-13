f76b93c0ee97a9f402cea138df36522ed9e6abbe
SGrottel <webmaster@sgrottel.de>
2021-05-13 20:21:42 +0200

    Setup appveyor scripts

---
+++ publish-release.ps1
@@ -0,0 +1,12 @@
+﻿param(
+	[Parameter(Mandatory=$true)][string]$version
+	)
+
+dotnet publish -c release /p:CopyOutputSymbolsToPublishDirectory=false
+
+# write-host $version
+rm ".\bin\Release\*.zip" -ErrorAction Ignore
+rm ".\bin\Release\Checkouts-Overview" -recurse -force -ErrorAction Ignore
+mkdir ".\bin\Release\Checkouts-Overview"
+copy ".\bin\Release\net5.0-windows\publish\*" ".\bin\Release\Checkouts-Overview" -recurse -force
+Compress-Archive -Path ".\bin\Release\Checkouts-Overview" -DestinationPath (".\bin\Release\Checkouts-Overview-" + $version + ".zip") -force

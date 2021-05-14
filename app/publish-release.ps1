4845391008972fdb0514385b3707bce0c0bff490
SGrottel <webmaster@sgrottel.de>
2021-05-14 11:26:51 +0200

    Fixing publish script execution after moving the app

--- app/publish-release.ps1
+++ app/publish-release.ps1
@@ -2,11 +2,11 @@
 	[Parameter(Mandatory=$true)][string]$version
 	)
 
-dotnet publish -c release /p:CopyOutputSymbolsToPublishDirectory=false
+dotnet publish Checkouts-Overview.sln -c release --no-build --no-restore /p:CopyOutputSymbolsToPublishDirectory=false
 
 # write-host $version
-rm ".\bin\Release\*.zip" -ErrorAction Ignore
-rm ".\bin\Release\Checkouts-Overview" -recurse -force -ErrorAction Ignore
-mkdir ".\bin\Release\Checkouts-Overview"
-copy ".\bin\Release\net5.0-windows\publish\*" ".\bin\Release\Checkouts-Overview" -recurse -force
-Compress-Archive -Path ".\bin\Release\Checkouts-Overview" -DestinationPath (".\bin\Release\Checkouts-Overview-" + $version + ".zip") -force
+rm "${PSScriptRoot}\bin\Release\*.zip" -ErrorAction Ignore
+rm "${PSScriptRoot}\bin\Release\Checkouts-Overview" -recurse -force -ErrorAction Ignore
+mkdir "${PSScriptRoot}\bin\Release\Checkouts-Overview"
+copy "${PSScriptRoot}\bin\Release\net5.0-windows\publish\*" "${PSScriptRoot}\bin\Release\Checkouts-Overview" -recurse -force
+Compress-Archive -Path "${PSScriptRoot}\bin\Release\Checkouts-Overview" -DestinationPath ("${PSScriptRoot}\bin\Release\Checkouts-Overview-" + $version + ".zip") -force

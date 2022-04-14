6f7921346aee2bf8f3de109cbb51ad0d59106dc3
SGrottel <webmaster@sgrottel.de>
2022-04-14 07:55:41 +0200

    Styled `SaveConfirmDialogWindow` replaces standard message box

---
+++ app/SaveConfirmDialogWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SG.Checkouts_Overview
+{
+    /// <summary>
+    /// Interaction logic for SaveConfirmDialogWindow.xaml
+    /// </summary>
+    public partial class SaveConfirmDialogWindow : Window
+    {
+        public MessageBoxResult Result { get; private set; } = MessageBoxResult.Cancel;
+
+        public SaveConfirmDialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSaveNow_Click(object sender, RoutedEventArgs e)
+        {
+            Result = MessageBoxResult.Yes;
+            Close();
+        }
+
+        private void ButtonContinueWithoutSave_Click(object sender, RoutedEventArgs e)
+        {
+            Result = MessageBoxResult.No;
+            Close();
+        }
+    }
+}

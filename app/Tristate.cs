5b44b69157a7d7046326531f17bfd8e26a2c1b8e
SGrottel <webmaster@sgrottel.de>
2022-05-14 08:25:22 +0200

    Added a per-Entry setting to control the `git fetch`

---
+++ app/Tristate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.Checkouts_Overview
+{
+
+    public enum Tristate
+    {
+        Default,
+        False,
+        True,
+    }
+
+}
